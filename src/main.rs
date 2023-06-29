2b8dcb059b2e5d0303f3a77e5495c61fb1fafa45
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-06-29 21:59:09 +0100

    Add new VGA Text Buffer code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-#![no_std] // don't link the Rust standard library.
-#![no_main] // disable all Rust-level entry points.
+#![no_std]
+#![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
